9e3603ca23c23aeb13abfeeee355eaa9dfd32db6
tejas <tejasshah@gmail.com>
2023-07-04 20:28:14 +0530

    add inorder

--- bst_tree.py
+++ bst_tree.py
@@ -19,4 +19,39 @@ root = None
 # tmp.left = None
 # tmp.right = None 
 
-print("\n\n1 For Add\n2 For Print\n0 Exit\n\nEnter choice")
+def addNode(root,data): #100,50 
+    if root == None:
+        root = Node()
+        root.data = data 
+        root.left = None
+        root.right = None 
+        return root 
+    elif root.data > data:
+        root.left = addNode(root.left,data)
+    else:
+        root.right = addNode(root.right,data)
+    return root 
+
+"""
+    tree travesal -> 3
+        1) inOrder    left:root:right {ascending order}
+        2) preOrder   root:left:right 
+        3) postOrder  left:right:root
+"""
+
+def inOrder(root):
+    if root != None:
+        inOrder(root.left) #100  
+        print(root.data)
+        inOrder(root.right)
+while True:
+    print("\n\n1 For Add\n2 For Print\n0 Exit\n\nEnter choice")
+    choice = int(input())
+
+    if choice == 1 : 
+        num = int(input())
+        root = addNode(root,num) #root{100},50
+    elif choice == 2:
+            inOrder(root)
+    elif choice ==0 :
+        exit(0)
